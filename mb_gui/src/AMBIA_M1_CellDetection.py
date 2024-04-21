issue with the rotation fixed

mb_gui/src/AMBIA_M1_CellDetection.py
@@ -6,12 +6,20 @@ from skimage.feature import blob_log
 import Switches_Static as st_switches
 from Switches_Static import rx,cx
 import time
+
+# Class to hold outputs; now using list as a class attribute rather than an instance attribute
 class OUTPUTS:
     OUTPUT=[]
+
+# Function to calculate Euclidean norms between an array and each row in another array
 def calculate_norms(arr,X):
   return np.linalg.norm(X-arr.reshape(1,arr.shape[0]),axis=1)
+
+# Function to check if any element in an array is below a threshold
 def filter (arr):
     return np.any(arr<6)
+
+# Main function to calculate colocalized blobs
 def calculate_colocalized_blobs(blobs_log):
     blobs_log_=[]
     colocalized_blobs=[]
@@ -34,13 +42,8 @@ def calculate_colocalized_blobs(blobs_log):
             colocalized_blobs_counts.append(0)
             continue
         #calculating the distance between every pair of cells
-        
         norms=np.apply_along_axis(calculate_norms,axis=1,arr=channel_to_start,X=blobs_log_[index2])
-       
         coords=np.apply_along_axis(filter,axis=1,arr=norms)
-        #print (coords)
-
-        
         channel_to_start=channel_to_start[coords]
         if len(i)>2:
             for j in i[2:]:
@@ -49,7 +52,6 @@ def calculate_colocalized_blobs(blobs_log):
                     norms=np.apply_along_axis(calculate_norms,axis=1,arr=channel_to_start,X=blobs_log_[index_])
                     coords=np.apply_along_axis(filter,axis=1,arr=norms)
                     channel_to_start=channel_to_start[coords]
-
                 except:
                     no_coloc=True
                     break
@@ -60,8 +62,6 @@ def calculate_colocalized_blobs(blobs_log):
             colocalized_blobs.append(list(channel_to_start))
             colocalized_blobs_counts.append(len(list(channel_to_start)))
     return colocalized_blobs_counts,colocalized_blobs
-
-
 
 
 def MoG_detection(img_channel, maxsigma, numsigma, thresh, brain_mask_eroded):
@@ -94,12 +94,11 @@ def cfos_detection(args):
     return (blobs_log,args[-2],args[-1])
     
 
-
-
 def log_result(result):
     OUTPUTS.OUTPUT.append(result)
-def pool_cell_detection(img_channel, brain_mask_eroded, minsigma, maxsigma, numsigma, blobs_thresh):
 
+
+def pool_cell_detection(img_channel, brain_mask_eroded, minsigma, maxsigma, numsigma, blobs_thresh):
     blobs_log = []
     OUTPUTS.OUTPUT.clear()
     r,c = img_channel.shape[0:2]

mb_gui/src/GuiFunctions.py
@@ -220,51 +220,33 @@ class Slide_Operator:
             os.makedirs(self.section_savepath)
         if self.slideformat == "mrxs":
             x, y, w, h = brainboundcoords[brnum0-1]  # mlevel
-            [xb, yb, wb, hb] = [x * self.dfmb, y * self.dfmb, w * self.dfmb, h * self.dfmb]
-            [xa, ya, wa, ha] = [x * self.dfma, y * self.dfma, w * self.dfma, h * self.dfma]
+            [_, _, wb, hb] = [x * self.dfmb, y * self.dfmb, w * self.dfmb, h * self.dfmb]
+            [_, _, wa, ha] = [x * self.dfma, y * self.dfma, w * self.dfma, h * self.dfma]
+
             Slide = openslide.OpenSlide(self.slidepath)
             Dims = Slide.level_dimensions
-            #if len (st_switches.num_channels)>
-            """try :
-                brainimg = Slide.read_region((y * self.dfm0, Dims[0][1] - ((x + w) * self.dfm0)), self.blevel, (hb, wb)).convert("BGR")#.convert("RGB")
-                brainimg2 = np.array(brainimg)
-            except : """
             brainimg = Slide.read_region((y * self.dfm0, Dims[0][1] - ((x + w) * self.dfm0)), self.blevel, (hb, wb))#.convert("BGR")#.convert("RGB")
             brainimg2 = cv.cvtColor(np.array(brainimg),cv.COLOR_BGR2RGB)
-            brainimg2=imgprc.rotate_by_angle(brainimg2,rotation_angle,b_level=True,b_level_scale=self.alevel-self.blevel)
-            #brainimg2 = cv.cvtColor(np.array(brainimg2),cv.COLOR_BGR2RGB)
-            """_=brainimg2[:,:,0]
-            brainimg2[:,:,0]=brainimg2[:,:,2]
-            brainimg2[:,:,2]=_"""
-            #brainimg2=cv.cvtColor(brainimg2,cv.COLOR_RGB2BGR)
-            #print (brainimg2.shape , "brainimgbrainimgbrainimgbrainimg",brainimg2[brainimg2>20].shape)
+            brainimg2 = imgprc.rotate_by_angle(brainimg2, rotation_angle, b_level=True, b_level_scale=self.alevel-self.blevel)
+
             if not st_switches.rotate_flag:
-    
                 section_blevel = cv.rotate(brainimg2, cv.ROTATE_90_CLOCKWISE)
             else : 
                 section_blevel = brainimg2
+
             if st_switches.color_switch_on:
                 section_blevel2 = section_blevel.copy()
                 section_blevel2[:,:,1]= section_blevel[:,:,2]
                 section_blevel2[:,:,2]= section_blevel[:,:,1]
                 section_blevel = section_blevel2
+                
             section_blevel_eq = equalize_img(section_blevel)
-            """ try :
-                braina = Slide.read_region((y * self.dfm0, Dims[0][1] - ((x + w) * self.dfm0)), self.alevel, (ha, wa)).convert("BGR")#.convert("RGB")
-                braina_dark = np.array(braina)
-                #braina_dark = cv.cvtColor(np.array(braina),cv.COLOR_RGB2BGR)
-            except : """
             braina = Slide.read_region((y * self.dfm0, Dims[0][1] - ((x + w) * self.dfm0)), self.alevel, (ha, wa))#.convert("BGR")#.convert("RGB")
             
             braina_dark = cv.cvtColor(np.array(braina),cv.COLOR_BGR2RGB)
             braina_dark=imgprc.rotate_by_angle(braina_dark,rotation_angle)
-            #braina_dark = cv.cvtColor(np.array(braina_dark),cv.COLOR_BGR2RGB)
-            """_=braina_dark[:,:,0]
-            braina_dark[:,:,0]=braina_dark[:,:,2]
-            braina_dark[:,:,2]=_"""
             #braina_dark=cv.cvtColor(braina_dark,cv.COLOR_RGB2BGR)
             if not st_switches.rotate_flag:
-
                 braina_rot = cv.rotate(braina_dark, cv.ROTATE_90_CLOCKWISE)
                 section_alevel = braina_rot
             else : 
@@ -275,27 +257,7 @@ class Slide_Operator:
                 section_alevel2[:,:,2]= section_alevel[:,:,1]
                 section_alevel = section_alevel2
             section_alevel_eq = equalize_img(section_alevel)
-            
-            #####
-            """blevelstack=[]
-            for index in range(section_blevel.shape[2]):
-                #channel_name = self.channel_types[channel]
-                #blevel_channel = self.czi.czi_section_img(self.slidepath, brnum0, num_sections, self.blevel, [channel], rect=None)
-                blevel_channel = section_blevel[..., index]
-                if st_switches.gammas[index]=="default":
-                    gamma_corrected_image = imgprc.gamma_correction(blevel_channel)
-                else:
-                    gamma_corrected_image = imgprc.gamma_correction(blevel_channel, st_switches.gammas[index])
-                #sharpened_image = imgprc.apply_sharpening(gamma_corrected_image)
-                #sharpened_image=cv.bitwise_and(gamma_corrected_image, gamma_corrected_image, mask = blevel_mask)
 
-                blevelstack.append(gamma_corrected_image)
-                if st_switches.rotate_flag:
-                    cv.imwrite(os.path.join(self.section_savepath, f"blevel_{self.channel_types[channel]}.png"), cv.rotate(sharpened_image, cv.ROTATE_90_CLOCKWISE))
-                    #cv.imwrite(os.path.join(self.section_savepath, f"blevel_{self.channel_types[channel]}.png"), cv.rotate(blevel_channel, cv.ROTATE_90_CLOCKWISE))
-
-            blevel_b, blevel_g, blevel_r =cv.split(section_blevel)# blevelstack#cv.split(section_blevel)
-            section_blevel_eq = czi_channel_regulator(np.dstack(blevelstack))"""
             #section_alevel_eq = equalize_img(section_alevel)
             blevel_b, blevel_g, blevel_r = cv.split(section_blevel)
             cv.imwrite(os.path.join(self.section_savepath,"alevel.png"), section_alevel)
@@ -314,8 +276,14 @@ class Slide_Operator:
             
             section_alevel = self.czi.czi_section_img(self.slidepath, brnum0, num_sections, self.alevel, st_switches.num_channels, rect=None)
             section_blevel = self.czi.czi_section_img(self.slidepath, brnum0, num_sections, self.blevel, st_switches.num_channels, rect=None)
-            section_alevel=imgprc.rotate_by_angle(section_alevel,rotation_angle)
-            section_blevel=imgprc.rotate_by_angle(section_blevel,rotation_angle)
+            print("+++++++++ alevel size: ", section_alevel.shape)
+            print("+++++++++ blevel size: ", section_blevel.shape)
+            section_alevel=imgprc.rotate_by_angle(section_alevel, rotation_angle)
+            section_blevel=imgprc.rotate_by_angle(section_blevel, rotation_angle, b_level=True, b_level_scale=self.alevel-self.blevel)
+            print("after rotation")
+            print("+++++++++ alevel size: ", section_alevel.shape)
+            print("+++++++++ blevel size: ", section_blevel.shape)
+
             if st_switches.rotate_flag:
                 section_alevel=cv.rotate(section_alevel, cv.ROTATE_90_CLOCKWISE)
                 section_blevel=cv.rotate(section_blevel, cv.ROTATE_90_CLOCKWISE)

mb_gui/src/utils/img_processing.py
@@ -2,50 +2,42 @@ import numpy as np
 from pathlib import Path
 import Switches_Static as st_switches
 from skimage import measure
-
 import cv2 as cv
+
 def check_switch(parameter):
     global tf
     if parameter.section_classifier_on or parameter.segmentation_1_20_on or parameter.section_QL_on:
         import tensorflow
         tf=tensorflow
-def rotate_by_angle(image, angle, center = None, scale = 1.0,b_level=False,b_level_scale=1):
+
+def rotate_by_angle(image, angle, center = None, scale = 1.0, b_level=False, b_level_scale=1):
         if b_level : 
             pad_size=st_switches.rotation_padding
             pad_size=pad_size *( 2**b_level_scale)
         else : pad_size=st_switches.rotation_padding
+
         (h, w) = image.shape[:2]
+
         upper_pad=np.zeros((pad_size,w,3),dtype=np.uint8)
         side_pad=np.zeros((h+(pad_size*2),pad_size,3),dtype=np.uint8)
         if st_switches.Bright_field:
             borderValue=(255,255,255)
             upper_pad+=255
             side_pad+=255
-            """#padded_image = cv.copyMakeBorder(image, st_switches.rotation_padding, st_switches.rotation_padding,
-                                                st_switches.rotation_padding, st_switches.rotation_padding, 
-                                                cv.BORDER_CONSTANT,(0,255,0))"""
         else :
             borderValue=(0,0,0)
-            """pad=(0,0,0)
-            padded_image = cv.copyMakeBorder(image, st_switches.rotation_padding, st_switches.rotation_padding,
-                                                st_switches.rotation_padding, st_switches.rotation_padding, 
-                                                cv.BORDER_CONSTANT,(0,0,0))
-        """
-        padded_image=np.concatenate((upper_pad,image,upper_pad),axis=0)
 
+        padded_image=np.concatenate((upper_pad,image,upper_pad),axis=0)
         padded_image=np.concatenate((side_pad,padded_image,side_pad),axis=1)
         
-        #h,w=h+(st_switches.rotation_padding*2),w+(st_switches.rotation_padding*2)
         (h, w) = padded_image.shape[:2]
         if center is None:
             center = (w / 2, h / 2)
-
-        # Perform the rotation
         
         M = cv.getRotationMatrix2D(center, angle, scale)
         rotated = cv.warpAffine(padded_image, M, (w, h) ,borderValue=borderValue)
-
         return rotated
+
 
 def rotate_img(img):
     img_rot = np.zeros((img.shape[1], img.shape[0], img.shape[2]))      ###########  optimize